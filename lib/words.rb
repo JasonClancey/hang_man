6ae297bce25c2a398c0862486522ced79a0ebe73
Jason Clancey <jason.clancey@dal.ca>
2022-10-05 22:56:07 -0600

    created lib directory

---
+++ lib/words.rb
@@ -0,0 +1,5 @@
+raw_words_list = File.readlines('google-10000-english-no-swears.txt')
+
+words_list = []
+
+raw_words_list.each {|word| if word.chomp.length >= 5 && word.length <= 12 then words_list.append(word) end}
