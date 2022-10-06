0e1ae4fe076d66a81b8e082f68336f8ccf37097d
Jason Clancey <jason.clancey@dal.ca>
2022-10-05 23:18:51 -0600

    created class, chooses a random word from the word list

--- lib/words.rb
+++ lib/words.rb
@@ -1,5 +1,11 @@
+class Words 
+
 raw_words_list = File.readlines('google-10000-english-no-swears.txt')
 
 words_list = []
 
-raw_words_list.each {|word| if word.chomp.length >= 5 && word.length <= 12 then words_list.append(word) end}
+raw_words_list.each { |word| if word.chomp.length >= 5 && word.length <= 12 then words_list.append(word) end }
+
+chosen_word = words_list.sample.split('')
+
+end
