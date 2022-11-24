165add845528e01d0183686ef9ccd7996ad0ff61
Jason Clancey <jason.clancey@dal.ca>
2022-11-23 22:19:11 -0700

    refactored code to a class, created function to choose word and guess a letter

--- lib/words.rb
+++ lib/words.rb
@@ -1,11 +1,21 @@
 class Words 
 
-raw_words_list = File.readlines('google-10000-english-no-swears.txt')
+    attr_reader :chosen_word
 
-words_list = []
+    def initialize
+        @chosen_word = ''
+        @guessed_letter = ''
+        @raw_words_array = File.readlines('google-10000-english-no-swears.txt')
+    end
 
-raw_words_list.each { |word| if word.chomp.length >= 5 && word.length <= 12 then words_list.append(word) end }
+    def choose_word
+        words_array = []
+        @raw_words_array.each { |word| if word.chomp.length >= 5 && word.chomp.length <= 12 then words_array.append(word) end }
+        @chosen_word = words_array.sample.split('')
+    end
 
-chosen_word = words_list.sample.split('')
-
+    def guess_letter
+        puts "Please enter a letter: "
+        @guessed_letter = gets.chomp.case
+    end
 end
