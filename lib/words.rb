7285c18ac6b6a6e776f573f3aa359c0f7d4ecd02
Jason Clancey <jason.clancey@dal.ca>
2022-11-28 21:52:52 -0700

    Added an incorrect guess counter, revised accessors.

--- lib/words.rb
+++ lib/words.rb
@@ -1,6 +1,8 @@
 class Words 
 
-    attr_reader :guessed__word_array
+
+    attr_reader :chosen_word_array, :guessed_word_array, :incorrect_letters_used
+
     require_relative 'display'
     include Display
 
@@ -10,13 +12,14 @@ class Words
         @guessed_word_array = []
         @chosen_word_array = []
         @raw_words_array = File.readlines('google-10000-english-no-swears.txt')
+        @incorrect_letters_used = 0
     end
 
     def choose_word
         words_array = []
         @raw_words_array.each { |word| if word.chomp.length >= 5 && word.chomp.length <= 12 then words_array.append(word.chomp.upcase) end }
         @chosen_word_array = words_array.sample.split('')
-        @chosen_word_array.length.times {@guessed_word_array.push('_')}
+        @chosen_word_array.length.times { @guessed_word_array.push('_') }
     end
 
     def guess_letter
@@ -40,11 +43,25 @@ class Words
                 @guessed_word_array[index] = @guessed_letter
             end
         end
+        if @chosen_word_array.include?(@guessed_letter) == false
+            @incorrect_letters_used +=1
+        end
         display_used_letters
+    end
+
+    #for testing only
+    def display_incorrect
+        puts @incorrect_letters_used
+        display_guessed_array
     end
 end
 
+# for puts debugging
 word = Words.new
 word.choose_word
+while true
 word.guess_letter
 word.check_guessed_letter
+word.display_incorrect
+
+end
