0a1eaae73f3bfe8acf8e5348fb90beefc5616061
Jason Clancey <jason.clancey@dal.ca>
2022-11-28 19:27:58 -0700

    Added basic display functions for requesting input, displaying the letters correctly guessed, and an error message for an invalid guess

--- lib/words.rb
+++ lib/words.rb
@@ -1,21 +1,42 @@
 class Words 
 
-    attr_reader :chosen_word
+    attr_reader :guessed_array
+    require_relative 'display'
+    include Display
 
     def initialize
-        @chosen_word = ''
         @guessed_letter = ''
+        @guessed_array = []
+        @chosen_word_array = []
         @raw_words_array = File.readlines('google-10000-english-no-swears.txt')
     end
 
     def choose_word
         words_array = []
-        @raw_words_array.each { |word| if word.chomp.length >= 5 && word.chomp.length <= 12 then words_array.append(word) end }
-        @chosen_word = words_array.sample.split('')
+        @raw_words_array.each { |word| if word.chomp.length >= 5 && word.chomp.length <= 12 then words_array.append(word.chomp.upcase) end }
+        @chosen_word_array = words_array.sample.split('')
+        @chosen_word_array.length.times {@guessed_array.push('_')}
     end
 
     def guess_letter
-        puts "Please enter a letter: "
-        @guessed_letter = gets.chomp.case
+        request_player_guess
+        @guessed_letter = gets.chomp.upcase
+        unless /[A-Z]/ =~ @guessed_letter
+            display_invalid_guess
+            guess_letter
+        end
+    end
+
+    def check_guessed_letter
+        @chosen_word_array.each_with_index do |letter, index|
+            if @guessed_letter == letter
+                @guessed_array[index] = @guessed_letter
+            end
+        end
     end
 end
+
+word = Words.new
+word.choose_word
+word.guess_letter
+word.check_guessed_letter
