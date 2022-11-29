2a3cdb607a4039dc4c3c5044d95c777e22d60f2c
Jason Clancey <jason.clancey@dal.ca>
2022-11-28 20:32:01 -0700

    Added variables to display the hanged man's, head arms, etc.

--- lib/display.rb
+++ lib/display.rb
@@ -1,7 +1,18 @@
 module Display
 
+    head = ''
+    body = ''
+    left_arm = ''
+    right_arm = ''
+    left_leg = ''
+    right_leg = ''
+
     def request_player_guess
         puts "Please enter a letter: "
+    end
+
+    def display_used_letters
+        puts "You have used these letters #{@used_letters.join(' ')}"
     end
 
     def display_guessed_array
