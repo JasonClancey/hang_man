6fe421060d9ec2a373035c13b9fa890411a0ccb0
Jason Clancey <jason.clancey@dal.ca>
2022-11-29 18:54:42 -0700

    Added the check guessed letter function

--- lib/game.rb
+++ lib/game.rb
@@ -5,9 +5,9 @@ class Game
     include Display
 
     def initialize
-        @hangman = Words.new
-        @game_over = false
+        @words = Words.new
         @turn = 0
+        @game_over = false
         @head = ' '
         @body = ' '
         @left_arm = ' '
@@ -16,11 +16,24 @@ class Game
         @right_leg = ' '
     end
 
+    def check_guessed_letter
+        @words.chosen_word_array.each_with_index do |letter, index|
+            if @words.guessed_letter == letter
+                @words.guessed_word_array[index] = @guessed_letter
+            end
+        end
+        if @words.chosen_word_array.include?(@guessed_letter) == false
+            @turn +=1
+            add_to_gallows
+        end
+        display_used_letters
+    end
+
     def check_if_winner
-        if @hangman.chosen_word_array.eql?(@hangman.guessed_word_array) && @turn <= 5
+        if @words.chosen_word_array.eql?(@words.guessed_word_array) && @words.incorrect_letters_used <= 5
             display_win
             @game_over = true
-        elsif @turn > 5
+        elsif @words.incorrect_letters_used > 5
             @game_over = true
         end
     end
@@ -39,6 +52,12 @@ class Game
             @left_leg = '/'
         when 6
             @right_leg = '\\'
+        end
+    end
+
+    def set_up_game
+        words.choose_word
+
     end
 
 end
