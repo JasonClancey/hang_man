a30e28eed936e89c01b7a3b4133f1ccf22700146
Jason Clancey <jason.clancey@dal.ca>
2022-11-29 19:42:03 -0700

    Added functions related to asking the player to play agai and, thank the player

--- lib/display.rb
+++ lib/display.rb
@@ -4,8 +4,9 @@ module Display
         puts <<~HEREDOC
         Welcome to hangman. This is a simple game where a word is chosen randomly.
         You must guess the word by entering letters when prompted.
-        You have 6 turns until the hanged man is fully drawn.
+        You have 6 incorrect guesses until the hanged man is fully drawn.
         Once the hanged man is fully draw, you lose.
+        If you guess the word with less than 6 incorrect guess you win!
         HEREDOC
     end
 
@@ -14,11 +15,12 @@ module Display
     end
 
     def display_used_letters
-        puts "You have used these letters: #{@used_letters.join(' ')}"
+        puts "You have used these letters: #{@words.used_letters.join(' ')}"
     end
 
     def display_guessed_array
-        puts @guessed_word_array.join(' ')
+        puts "The word to guess:"
+        puts @words.guessed_word_array.join(' ')
     end
 
     def display_invalid_guess
@@ -30,18 +32,35 @@ module Display
         +---+
         |   |
         #{@head}   |
-        #{@left_arm}#{@body}#{@right_arm} |
-        #{@left_leg}#{@right_leg}  |
+       #{@left_arm}#{@body}#{@right_arm}  |
+       #{@left_leg} #{@right_leg}  |
             |
       ============
       HEREDOC
     end
 
     def display_win
-        puts "Congratulations! You have correctly guessed the word."
+        puts 'Congratulations! You have correctly guessed the word.'
     end
 
     def display_loss
-        puts "Sorry! You have not guessed the word in"
+        puts 'Sorry! You have not guessed the word successfully!'
+    end
+
+    def display_the_word
+        puts 'The correct word was:'
+        puts @words.chosen_word_array.join(' ')
+    end
+
+    def display_play_again
+        puts 'Would you like to play again? Y/N'
+    end
+
+    def display_play_again_invalid
+        puts 'Invalid entry. Please try again.'
+    end
+
+    def display_thanks
+        puts 'Thanks for playing!'
     end
 end
