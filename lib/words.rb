e163760570e475fefe1e41dacd13c8ba6d74336c
Jason Clancey <jason.clancey@dal.ca>
2022-12-01 11:14:15 -0700

    moved the guess letter and check guessed letter functions to game

--- lib/words.rb
+++ lib/words.rb
@@ -1,15 +1,16 @@
 class Words 
 
-    attr_reader :chosen_word_array, :guessed_word_array, :incorrect_letters_used, :guessed_letter, :used_letters
+    attr_accessor :used_letters, :guessed_word_array, :chosen_word_array, :guessed_letter
 
     require_relative 'display'
+
     include Display
 
-    def initialize
-        @used_letters = []
+    def initialize(used_letters = [], guessed_word_array = [], chosen_word_array = [])
+        @used_letters = used_letters
         @guessed_letter = ''
-        @guessed_word_array = []
-        @chosen_word_array = []
+        @guessed_word_array = guessed_word_array
+        @chosen_word_array = chosen_word_array
         @raw_words_array = File.readlines('google-10000-english-no-swears.txt')
     end
 
@@ -21,19 +22,5 @@ class Words
         @raw_words_array = []
     end
 
-    def guess_letter
-        active = true
-        request_player_guess
-        @guessed_letter = gets.chomp.upcase
-            while active
-                if /[A-Z]/ =~ @guessed_letter && @used_letters.include?(@guessed_letter) == false && @guessed_letter.length == 1
-                    active = false
-                else
-                    display_invalid_guess
-                    @guessed_letter = gets.chomp.upcase
-                end
-            end
-        @used_letters.push(@guessed_letter)
-    end
 end
 
